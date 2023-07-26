9325ef01ccc179d1e71979c58a007e489a5a959b
ahmadhatahet <ahmad.hatahet@hotmail.com>
2023-07-26 15:14:48 +0200

    add train acc and loss

--- train-one-model.py
+++ train-one-model.py
@@ -7,18 +7,10 @@
 # # Setup Variables
 
 # MNIST, FashionMNIST, GTSRB, Cifar10
-
-# In[1]:
-
-
-DATASET = 'Cifar10'
+DATASET = 'MNIST'
 SEED = 42
 CUDA = 0
-LHL = 30
 GPU_NAME = f'cuda:{CUDA}'
-
-
-# In[2]:
 
 
 import os
@@ -32,38 +24,19 @@ if base.name != 'runtime-monitoring':
 
 
 # # Libraries
-
-# In[3]:
-
-
-import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
-import json
-from fastprogress import progress_bar, master_bar
-
 import torch
-import torchvision
 import torch.nn as nn
 import torch.backends.cudnn as cudnn
-import torch.nn.functional as F
-from torchinfo import summary
 
 
 cudnn.benchmark = True
 torch.set_float32_matmul_precision('high')
 
-
-# In[4]:
-
-
 from utilities.utils import *
 from utilities.pathManager import fetchPaths
 from utilities.scaleFunctions import *
 from utilities.pcaFunctions import *
-
-
-# In[5]:
 
 
 # disable warnings
@@ -72,9 +45,6 @@ warnings.filterwarnings('ignore')
 
 
 # # Paths
-
-# In[6]:
-
 
 paths = fetchPaths(base, DATASET, '', False)
 path_data = paths['data']
@@ -87,18 +57,9 @@ optim_args = config['optimizer'][optim_name]
 scheduler_name = list(config['scheduler'].keys())[0]
 scheduler_args = config['scheduler'][scheduler_name]
 
-# # GPU Device & Seed
-
-# In[7]:
-
-
 np.random.seed(SEED)
 torch.manual_seed(SEED)
 torch.cuda.manual_seed(SEED)
-
-
-# In[8]:
-
 
 device = get_device(GPU_NAME)
 torch.cuda.get_device_name(device)
@@ -106,16 +67,12 @@ torch.cuda.get_device_name(device)
 
 # ## Load model and settings
 
-# In[9]:
-
-
 # transformers
 from models.transformers import transformers
 tf_train = transformers[DATASET.lower()]['train']
 tf_test = transformers[DATASET.lower()]['test']
 
 
-# In[10]:
 from models.mnist_model import MNIST_Model
 from models.fashionmnist_model import FashionMNIST_CNN
 from models.gtsrb_model import GTSRB_CNN
@@ -133,32 +90,21 @@ models = {
 model_ = models[DATASET.lower()]
 
 # # Load / Split / DataLoader
-
-# In[12]:
-
-
 feature_names = get_labels(DATASET)
 
 train_data = get_dataset(DATASET, path_data, train=True, transform=tf_train)
 test_data = get_dataset(DATASET, path_data, train=False, transform=tf_test)
-len(train_data), len(test_data)
 
-
-# In[13]:
 trainloader = get_dataLoader(train_data, model_config['batch_size'], True)
 testloader = get_dataLoader(test_data, model_config['batch_size'], False)
 
 
 # # Helper Functions
-model_setup['last_hidden_neurons'] = LHL
+model_setup['last_hidden_neurons'] = min(config['lhl_neurons'])
 
 model = model_(**model_setup).to(device)
 model = torch.compile(model)
 nn.DataParallel(model, device_ids=[CUDA])
-
-
-# In[19]:
-
 
 # loss function
 loss_function = nn.CrossEntropyLoss()
@@ -166,9 +112,6 @@ loss_function = nn.CrossEntropyLoss()
 # optimizer and scheduler
 optimizer = getattr(torch.optim, optim_name)(model.parameters(), lr=model_config['lr'], **optim_args)
 scheduler = getattr(torch.optim.lr_scheduler, scheduler_name)(optimizer, **scheduler_args)
-
-
-# In[20]:
 
 
 # training testing attributes
@@ -189,10 +132,18 @@ kwargs = {
 
 # ## Run Training
 
-# In[ ]:
-
-
 # train
-train_losses, test_losses, train_accs, test_accs, test_loss, test_acc, confusion_matrix_test, best_model_name = \
+
+(train_losses, train_accs,
+ test_losses,test_accs,
+ train_loss,train_acc
+ ,test_loss,test_acc,
+ confusion_matrix_train,
+ confusion_matrix_test,
+ best_model_name) = \
 run_training_testing(**kwargs)
 
+idx = np.argmax(test_accs)
+test_loss, test_acc = test_losses[idx], test_accs[idx]
+
+print(f'Done, Best accuracy: {test_acc*100:.2f}%, Best loss: {test_loss*100:.2f}')
