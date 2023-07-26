ca2a6d3b7c5c956ecebe71e8b351c54983dc78e6
ahmadhatahet <ahmad.hatahet@hotmail.com>
2023-07-26 15:12:55 +0200

    exclude pca and selected neurons combination

--- utilities/bdd_run_parallel.py
+++ utilities/bdd_run_parallel.py
@@ -7,6 +7,10 @@ from utilities.bdd_parallel_thresholds import run_parallel
 
 def run(dataset, postfix, subset_neurons, flavors, lhl_neurons, eta, memory):
     for FLAVOR, LOAD_NEURONS, N in product(flavors, subset_neurons, lhl_neurons):
+
+        # skip pca and selected neurons
+        if FLAVOR == 'scaler_pca' and LOAD_NEURONS: continue
+
         print(f'Start Time: {dt.now()}')
         print(dataset, postfix, N, FLAVOR, LOAD_NEURONS, eta)
 
