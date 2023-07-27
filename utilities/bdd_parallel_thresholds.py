3914baada0762b85ce1c09f30cec5e9977320bec
ahmadhatahet <ahmad.hatahet@hotmail.com>
2023-07-27 13:34:08 +0200

    load target dataset to evaluate on model and score against bdd

--- utilities/bdd_parallel_thresholds.py
+++ utilities/bdd_parallel_thresholds.py
@@ -3,35 +3,105 @@ import pandas as pd
 import numpy as np
 from pathlib import Path
 
-from utilities.utils import load_json, load_pickle
+from utilities.utils import *
 from utilities.pathManager import fetchPaths
+from utilities.pcaFunctions import applyPCASingle
+from utilities.scaleFunctions import applyStandardScalerSingle
 from utilities.MonitorUnifiedBDD import build_bdd_multi_etas
 
+from models.mnist_model import MNIST_Model
+from models.fashionmnist_model import FashionMNIST_CNN
+from models.gtsrb_model import GTSRB_CNN
+from models.cifar10_dla import Cifar10_DLA
+from models.cifar10_model import Cifar10_CNN
+
+from models.transformers import transformers
+
+models = {
+    'mnist': MNIST_Model,
+    'fashionmnist': FashionMNIST_CNN,
+    'gtsrb': GTSRB_CNN,
+#     'cifar10': Cifar10_DLA,
+    'cifar10': Cifar10_CNN
+}
 
 def run_parallel(args):
 
-    DATASET, POSTFIX, N, FALVOR, LOAD_NEURONS, eta, memory = args
+    DATASET, TARGET_DATASET, POSTFIX, N, FALVOR, LOAD_NEURONS, eta, memory = args
 
     POSTFIX = f"{POSTFIX}-{N}"
 
     # env variables
     FILENAME_POSTFIX = f"{DATASET}_{POSTFIX}"
 
-    # paths
+    # generate paths
     base = Path().cwd()
     paths = fetchPaths(base, DATASET, POSTFIX)
     path_lhl = paths['lhl']
     path_lhl_data = paths['lhl_' + FALVOR]
     path_bdd = paths['bdd'] / FALVOR
     path_bdd.mkdir(exist_ok=True)
+    path_target_data = paths['data'].parent / TARGET_DATASET
 
     configs = load_json(paths['configuration'].parent / 'thresholds.json')
     thresholds = configs['thlds']
 
-    # load pca components
-    pca_ = None
-    if FALVOR != 'raw' :
-        pca_ = load_pickle(path_lhl / f'{FALVOR}.pkl')
+    # load config file
+    configs = load_json(paths['configuration'])
+    config = configs['configuration']
+
+    # model config
+    model_setup = configs['model_setup']
+    model_config = configs['model_config']
+
+    # saved model
+    path_model = next(paths['saved_models'].glob('*.pth.tar'))
+
+    # load trained model
+    model_ = models[DATASET.lower()]
+    transformer = transformers[DATASET.lower()]
+
+    # device
+    CUDA = 0
+    GPU_NAME = f'cuda:{CUDA}'
+    device = get_device(GPU_NAME)
+    torch.cuda.get_device_name(device)
+
+    # load test data
+    test_data = get_dataset(TARGET_DATASET, path_target_data, train=False)
+    mean_, std_ = transformer['test'].transforms[1].mean, transformer['test'].transforms[1].std
+    x_target = torch.stack([test_data[i][0] for i in test_sample]) - mean_ / std_
+
+
+    # torch 2.0 compile and parallel data training
+    model_setup['last_hidden_neurons'] = N
+    model = model_(**model_setup).to(device)
+    model = torch.compile(model)
+
+    # load model weights
+    model.load_state_dict(torch.load(path_model)['model'])
+
+    # eavluation mode
+    model.eval()
+
+    # get last hidden layer logits for target data
+    logits_target, _ = model.output_last_layer(x_target.to(device))
+    logits_target = logits_target.cpu().numpy()
+    logits_target = pd.DataFrame(logits_target, columns=[f'x{i}' for i in range(logits_target.shape[1])])
+
+    # project data to pca
+    if FALVOR == 'scaler_pca':
+        # load sacler object
+        sacler_ = load_pickle(path_lhl / f'scaler.pkl')
+        # load pca object
+        pca_ = load_pickle(path_lhl / f'scaler_pca.pkl')
+        # transform data
+        logits_target = applyPCASingle(logits_target, sacler_, pca_, N)
+
+    # target data
+    df_logits_copy = logits_target.copy()
+    df_logits_copy['y'] = 1
+    df_logits_copy['true'] = 1
 
 
     # output file name
@@ -39,52 +109,44 @@ def run_parallel(args):
     POSTFIX2 += "_neurons" if LOAD_NEURONS else ""
     POSTFIX2 += f"_{FILENAME_POSTFIX}"
 
-    # import Data
-    df_train_original = pd.read_csv(path_lhl_data / f"{FILENAME_POSTFIX}_{FALVOR}_train.csv")
+    # import lasthidden layer data
+    df_train = pd.read_csv(path_lhl_data / f"{FILENAME_POSTFIX}_{FALVOR}_train.csv")
+    df_test = pd.read_csv(path_lhl_data / f"{FILENAME_POSTFIX}_{FALVOR}_test.csv")
 
-    # select only true classified
-    df_true_original = df_train_original[df_train_original["true"] == True].copy()
-    df_true_original = df_true_original.drop("true", axis=1).reset_index(drop=True)
+    # select only true classified instances
+    df_true = df_train[df_train["true"] == True].copy()
+    df_true = df_true.drop("true", axis=1).reset_index(drop=True)
 
-    df_test_original = pd.read_csv(path_lhl_data / f"{FILENAME_POSTFIX}_{FALVOR}_test.csv")
-
+    # load selected neurons
     neurons_dict = {'None': []}
     if LOAD_NEURONS:
             neurons_dict['gte_mean'] = load_json(path_lhl / "neurons_scaler_pca_gte_mean.json")
             neurons_dict['top_third'] = load_json(path_lhl / "neurons_scaler_pca_top_third.json")
             del neurons_dict['None']
 
-    # if LOAD_NEURONS and pca_ is not None:
-    #     NUM_COMPONENTS = numComponents(pca_)
-    #     neurons = [f'x{i}' for i in range(NUM_COMPONENTS)]
-        # df_train.drop(df_train.columns[NUM_COMPONENTS+1:-2], axis=1, inplace=True)
-        # df_test.drop(df_train.columns[NUM_COMPONENTS+1:-2], axis=1, inplace=True)
-        # # true column is dropped, thus only y need to be kept
-        # df_true.drop(df_train.columns[NUM_COMPONENTS+1:-1], axis=1, inplace=True)
-
-
     # define thresholds
     thlds = []
     thld_names = []
 
     for p in thresholds:
-        if p != 0:
-            # generate odd quantiles
-            thlds.append(np.quantile(df_true_original.drop("y", axis=1), np.round(p, 1), axis=0))
-            thld_names.append(f"qth_{p}")
-        else:
-            # add zero (ReLU) quantile
-            thlds.append(np.zeros(df_true_original.drop("y", axis=1).shape[1]))
-            thld_names.append("relu")
+        # round number
+        p = np.round(p, 1)
+        # generate odd quantiles
+        thlds.append(np.quantile(df_true.drop("y", axis=1), p, axis=0))
+        thld_names.append(f"qth_{p}")
+
+    # add zero (ReLU)
+    thlds.append(np.zeros(df_true.drop("y", axis=1).shape[1]))
+    thld_names.append("relu")
+
+    # add mean
+    thlds.append(np.mean(df_true.drop("y", axis=1).shape[1], axis=0))
+    thld_names.append("mean")
 
 
     # loop over two types of selected neurons if LOAD_NEURONS
     for neuron_name, selected_neurons in neurons_dict.items():
-
-        df_train = df_train_original.copy(deep=True)
-        df_true = df_true_original.copy(deep=True)
-        df_test = df_test_original.copy(deep=True)
-
+        # replace neurons with type of subset
         if LOAD_NEURONS:
             POSTFIX2_TEMP = POSTFIX2.replace('neurons', neuron_name)
         else:
@@ -119,7 +181,7 @@ def run_parallel(args):
 
             results = pool.map(
                 build_bdd_multi_etas,
-                [(df_train.copy(deep=True), df_test.copy(deep=True), df_true.copy(deep=True),
+                [(df_train.copy(), df_test.copy(), df_true.copy(), df_logits_copy.copy(),
                 selected_neurons, thld_name, thld, eta, memory, path_bdd)
                 for thld_name, thld, eta in combinations])
 
