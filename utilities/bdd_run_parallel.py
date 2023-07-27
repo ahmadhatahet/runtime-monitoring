11d1ccd154a83a14bfb1f5413f0a54f3f9c5e7bd
ahmadhatahet <ahmad.hatahet@hotmail.com>
2023-07-27 13:32:06 +0200

    add which target dataset to which train one

--- utilities/bdd_run_parallel.py
+++ utilities/bdd_run_parallel.py
@@ -4,9 +4,14 @@ from itertools import product
 from utilities.utils import *
 from utilities.bdd_parallel_thresholds import run_parallel
 
+targets_datasets = {
+    'MNIST': 'FashionMNIST',
+    'FashionMNIST': 'MNIST',
+    'GTSRB': 'Cifar10',
+}
 
 def run(dataset, postfix, subset_neurons, flavors, lhl_neurons, eta, memory):
-    for FLAVOR, LOAD_NEURONS, N in product(flavors, subset_neurons, lhl_neurons):
+    for N, LOAD_NEURONS, FLAVOR in product(lhl_neurons, subset_neurons, flavors):
 
         # skip pca and selected neurons
         if FLAVOR == 'scaler_pca' and LOAD_NEURONS: continue
@@ -14,7 +19,7 @@ def run(dataset, postfix, subset_neurons, flavors, lhl_neurons, eta, memory):
         print(f'Start Time: {dt.now()}')
         print(dataset, postfix, N, FLAVOR, LOAD_NEURONS, eta)
 
-        run_parallel((dataset, postfix, N, FLAVOR, LOAD_NEURONS, eta, memory))
+        run_parallel((dataset, targets_datasets[dataset],postfix, N, FLAVOR, LOAD_NEURONS, eta, memory))
         gc.collect()
 
 
