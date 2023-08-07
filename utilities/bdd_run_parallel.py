6033b225b96ec7d345d2a66dac5033c662466a68
ahmadhatahet <ahmad.hatahet@hotmail.com>
2023-08-07 12:42:23 +0200

    remove target dataset

--- utilities/bdd_run_parallel.py
+++ utilities/bdd_run_parallel.py
@@ -4,11 +4,6 @@ from itertools import product
 from utilities.utils import *
 from utilities.bdd_parallel_thresholds import run_parallel
 
-targets_datasets = {
-    'MNIST': 'FashionMNIST',
-    'FashionMNIST': 'MNIST',
-    'GTSRB': 'Cifar10',
-}
 
 def run(dataset, postfix, subset_neurons, flavors, lhl_neurons, eta, memory):
     for N, LOAD_NEURONS, FLAVOR in product(lhl_neurons, subset_neurons, flavors):
@@ -19,7 +14,7 @@ def run(dataset, postfix, subset_neurons, flavors, lhl_neurons, eta, memory):
         print(f'Start Time: {dt.now()}')
         print(dataset, postfix, N, FLAVOR, LOAD_NEURONS, eta)
 
-        run_parallel((dataset, targets_datasets[dataset],postfix, N, FLAVOR, LOAD_NEURONS, eta, memory))
+        run_parallel((dataset, postfix, N, FLAVOR, LOAD_NEURONS, eta, memory))
         gc.collect()
 
 
